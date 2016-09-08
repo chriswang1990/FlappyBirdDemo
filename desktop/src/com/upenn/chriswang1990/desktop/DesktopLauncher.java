96e50878d0c978c1617879eedcc7784498ce7777
chriswang1990 <1990chriswang1990@gmail.com>
2016-09-08 14:13:53 -0700

    Initial commit for Flappy Bird Demo Project (Java and LibGDX)

---
+++ desktop/src/com/upenn/chriswang1990/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.upenn.chriswang1990.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.upenn.chriswang1990.FlappyBirdDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBirdDemo(), config);
+	}
+}
