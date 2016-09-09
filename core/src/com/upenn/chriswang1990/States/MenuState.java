125fb79ebc30a056345422c69846157aef841281
chriswang1990 <1990chriswang1990@gmail.com>
2016-09-09 01:30:31 -0700

    Add MenuState, use GSM in render and add all needed assets

---
+++ core/src/com/upenn/chriswang1990/States/MenuState.java
@@ -0,0 +1,25 @@
+package com.upenn.chriswang1990.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State{
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
