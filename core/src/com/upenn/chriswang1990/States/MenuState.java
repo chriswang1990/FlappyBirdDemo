348a1a28d947c6c974e4fc4d24a3095bd37d7ea0
chriswang1990 <1990chriswang1990@gmail.com>
2016-09-10 01:30:12 -0700

    Add background and playBtn to MenuState

--- core/src/com/upenn/chriswang1990/States/MenuState.java
+++ core/src/com/upenn/chriswang1990/States/MenuState.java
@@ -1,11 +1,18 @@
 package com.upenn.chriswang1990.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.upenn.chriswang1990.FlappyBirdDemo;
 
 public class MenuState extends State{
 
+    private Texture background;
+    private Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
     }
 
     @Override
@@ -20,6 +27,9 @@ public class MenuState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBirdDemo.WIDTH, FlappyBirdDemo.HEIGHT);
+        sb.draw(playBtn, (FlappyBirdDemo.WIDTH - playBtn.getWidth()) / 2, FlappyBirdDemo.HEIGHT / 2);
+        sb.end();
     }
 }
