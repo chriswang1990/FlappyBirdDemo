49f7540124df6acab8e5ef1b8eb445ff85ab894c
chriswang1990 <1990chriswang1990@gmail.com>
2016-09-10 10:04:56 -0700

    Add tubes and draw in play state

--- core/src/com/upenn/chriswang1990/sprites/Tube.java
+++ core/src/com/upenn/chriswang1990/sprites/Tube.java
@@ -1,4 +1,41 @@
 package com.upenn.chriswang1990.sprites;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
 public class Tube {
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBotTube;
+    private Random rand;
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+
+    public Tube (float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomTube.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+    }
 }
--- core/src/com/upenn/chriswang1990/states/PlayState.java
+++ core/src/com/upenn/chriswang1990/states/PlayState.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.upenn.chriswang1990.FlappyBirdDemo;
 import com.upenn.chriswang1990.sprites.Bird;
+import com.upenn.chriswang1990.sprites.Tube;
 
 public class PlayState extends State{
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyBirdDemo.WIDTH / 2, FlappyBirdDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -36,6 +39,8 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
